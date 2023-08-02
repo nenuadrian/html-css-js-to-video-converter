daa249bf955a9e4629d56a5a954fea873f362924
Nenu Adrian <adrian.nenu@gmail.com>
2023-08-02 12:07:23 +0100

    v1

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import puppeteer from 'puppeteer';
+import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder';
+
+(async () => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const recorder = new PuppeteerScreenRecorder(page);
+  // Navigate the page to a URL
+  await page.goto('https://webflow.com/interactions-animations');
+
+  // Set screen size
+  await page.setViewport({width: 1080, height: 768});
+  await recorder.start('demo.mp4');
+
+  await new Promise((resolve) => {
+    setTimeout(() => resolve(null), 5000);
+  })
+  // Type into search box
+  // Wait and click on first result
+
+  // Locate the ful
+  // Print the full title
+  await recorder.stop();
+
+  await browser.close();
+})();
